3e6ae778fbd41940cf531c64cf92de59d6eea4c0
dimon271086 <77069068+dimon271086@users.noreply.github.com>
2023-07-24 10:47:03 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function calculate() {
+	const type = document.getElementById('type').value;
+	const area = parseFloat(document.getElementById('area').value);
+	const thickness = parseFloat(document.getElementById('thickness').value);
+ 
+	if (isNaN(area) || isNaN(thickness)) {
+	  alert('Пожалуйста, введите корректные значения площади и толщины слоя.');
+	  return;
+	}
+ 
+	let result;
+	switch (type) {
+	  case 'цементно-песчаная':
+		 result = calculateCementSandMix(area, thickness);
+		 break;
+	  case 'штукатурная':
+		 result = calculatePlasterMix(area, thickness);
+		 break;
+	  case 'кладочная':
+		 result = calculateMasonryMix(area, thickness);
+		 break;
+	  default:
+		 result = 'Выберите тип строительной смеси.';
+	}
+ 
+	document.getElementById('result').innerText = `Необходимое количество смеси: ${result} кг`;
+ }
+ 
+ function calculateCementSandMix(area, thickness) {
+	// Ваш код для расчета количества смеси цементно-песчаной
+	// Например, пусть это будет простейший расчет: площадь * толщину слоя * коэффициент
+	const coefficient = 0.02; // Примерный коэффициент для цементно-песчаной смеси
+	return (area * thickness * coefficient).toFixed(2);
+ }
+ 
+ function calculatePlasterMix(area, thickness) {
+	// Ваш код для расчета количества штукатурной смеси
+	// Аналогично примеру выше
+	const coefficient = 0.03; // Примерный коэффициент для штукатурной смеси
+	return (area * thickness * coefficient).toFixed(2);
+ }
+ 
+ function calculateMasonryMix(area, thickness) {
+	// Ваш код для расчета количества кладочной смеси
+	// Аналогично примерам выше
+	const coefficient = 0.025; // Примерный коэффициент для кладочной смеси
+	return (area * thickness * coefficient).toFixed(2);
+ }
+ 
